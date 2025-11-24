db55104aedf3fe605956cba7f55890094b07bb35
jthw1005 <hjt105@gmail.com>
2025-11-25 00:46:50 +0900

    chore: tailwindcss, prettier 설치 및 환경 설정

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -1,19 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    css: true,
-  },
-})
+export default mergeConfig(
+	defineConfig({
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
+		},
+	}),
+	defineTestConfig({
+		test: {
+			globals: true,
+			environment: 'jsdom',
+			setupFiles: './src/test/setup.ts',
+			css: true,
+		},
+	}),
+);
